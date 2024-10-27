1dfd40909c9d24008800e47f081a55b4c72ca971
jeaq's g5 <julioeaq01@outlook.com>
2024-10-27 00:50:42 -0400

    upaded styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,93 +1,17 @@
-import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, useMutation, gql } from '@apollo/client';
-import { io } from 'socket.io-client';
-import { useState, useEffect } from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { useState,  } from 'react';
 import './App.css'; 
+import './styles/styles.css'
 import './pages/Login/Login';
 import Chat from './pages/Chat/Chat';
 import Login from './pages/Login/Login';
 
-const socket = io('http://localhost:1000');
-
-
 const client = new ApolloClient({
-    uri: 'http://localhost:1000/graphql',
+    uri: 'http://localhost:10000/graphql',
     cache: new InMemoryCache(),
 });
 
 
-
-
-// const Chat = () => {
-//     const { loading, error, data, refetch } = useQuery(GET_MESSAGES);
-//     const [addMessage] = useMutation(ADD_MESSAGE);
-//     const [register] = useMutation(REGISTER);
-//     const [login] = useMutation(LOGIN);
-//     const [newMessage, setNewMessage] = useState('');
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [token, setToken] = useState(null);  
-    
-//     useEffect(() => {                
-//         socket.on('connect', () => {            
-//         });
-//         socket.on('newMessage', (message) => {    
-//             refetch();                    
-//         });
-//         return () => {
-//             socket.disconnect();
-//         };
-//     }, []);
-
-//     const handleSend = async () => {
-//         const message = { message: newMessage, username: username };        
-//         socket.emit('sendMessage', message);
-//         await addMessage({ variables: message });
-//         setNewMessage('');
-//     };
-
-//     const handleRegister = async () => {
-//         var response = await register({ variables: { username, password } });
-//         console.log(response)
-//     };
-
-//     const handleLogin = async () => {
-//         const { data } = await login({ variables: { username, password } });
-//         console.log(data)
-//         setToken(data.login.token);
-//     };
-
-//     if (loading) return <p>Loading...</p>;
-//     if (error){      
-//       return <p>Error!</p>;
-//     } 
-
-//     return (
-//         // <div className="chat-container">
-//         //     {token ? 
-//         //     // (
-
-//         //         // <div>
-//         //         //     <div className="message-list">
-//         //         //         {data.messages.map(message => (
-//         //         //             <div key={message.id}>
-//         //         //                 <strong>{message.username}:</strong> {message.message}
-//         //         //             </div>
-//         //         //         ))}
-//         //         //     </div>
-//         //         //     <input
-//         //         //         value={newMessage}
-//         //         //         onChange={(e) => setNewMessage(e.target.value)}
-//         //         //         placeholder="Type a message"
-//         //         //     />
-//         //         //     <button onClick={handleSend}>Send</button>
-//         //         // </div>
-//         //     // )
-//         //      : 
-//         //         <Login></Login>
-//         //     }
-//         // </div>
-//     );
-// };
 
 const App = () => {   
     const [token, setToken] = useState(null);  
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const io = socketIo(server, {
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
